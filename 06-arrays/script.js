70fc1c5c266478004f7cc82bbb8478a2b7d5f332
Johan Nordström <johan@digitalvillage.se>
2024-10-24 12:07:09 +0200

    Array basics, part 1.

---
+++ 06-arrays/script.js
@@ -0,0 +1,35 @@
+/**
+ * Arrays
+ */
+
+// 🥴
+let points1 = 1;
+let points2 = 42;
+let points3 = 1337;
+let points4 = 999;
+
+// 🤩
+let points = [1, 42, 1337, 999];
+let students = [
+	"Deadpool",			// 0
+	"Black Widow",		// 1
+	"Iron Man",			// 2
+	"Spider-Man",		// 3
+];
+
+console.log("Students:", students);
+console.log("The second student's name is:", students[1]);
+
+console.log(`We have ${students.length} students in our class.`);
+
+/**
+ * Non-destructive methods
+ */
+
+// Join all items in an array to a string
+let names = students.join(", ");
+console.log("The students names are: " + names);
+
+// Find position (index) of string "Iron Man"
+console.log("Index of Iron Man:", students.indexOf("Iron Man"));  // 2
+console.log("Index of Thanos:", students.indexOf("Thanos"));  // -1
