c5113667165f6e4cc3e9840a756a70de80faba49
Johan Nordström <johan@digitalvillage.se>
2024-10-31 15:25:48 +0100

    Math + micro-workshop.

---
+++ 17-math/script.js
@@ -0,0 +1,31 @@
+/**
+ * Math 🧮
+ *
+ * .round() - Round a floating number to nearest integer
+ * .floor() - Round a floating number DOWN to nearest integer
+ * .ceil() - Round a floating number UP to nearest integer
+ *
+ * TODO:
+ * getRandomNumber ska kunna generera ett tal mellan 1 - X,
+ * till exempel ska getRandomNumber(20) ge ett tal mellan 1-20,
+ * getRandomNumber(50) ska ge ett tal mellan 1-50 och
+ * getRandomNumber() ska ge ett tal mellan 1-10
+ *
+ */
+
+const floatBetweenZeroAndAlmostTen = Math.random() * 10;   // 0.0000001 - 9.9999999
+const integerBetweenZeroAndTen = Math.round(floatBetweenZeroAndAlmostTen);  // 0 - 10
+
+// ONLY get number between 1 - 10
+const integerBetweenOneAndTen = Math.ceil( Math.random() * 10 );  // 1 - 10
+
+// Generate a random number between 1 - 10
+const getRandomNumber = () => {
+	return Math.floor( Math.random() * 10 ) + 1;
+}
+
+let numbers = [];
+for (let i = 0; i < 50; i++) {
+	numbers.push( getRandomNumber() );
+}
+console.log("Numbers:", numbers.join(", "));
