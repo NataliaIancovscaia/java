f0e6c74bbc5532c7147bf0e568c7fb09068d9d97
Johan Nordström <johan@digitalvillage.se>
2024-11-05 10:48:35 +0100

    Callbacks and looping over an array with `forEach()`.

--- 15-functions/script.js
+++ 15-functions/script.js
@@ -83,6 +83,7 @@ if (sum3 > 100) {
 console.log("Circumference of box with height 20 and width 40 is:", calcBoxCircumference(40, 20));
 */
 
+/*
 // Arrow Function (Fat Arrow Function)
 // (is still a function expression)
 
@@ -93,3 +94,98 @@ let boxArea1 = calcBoxArea(20, 40);  // let boxArea1 = 800
 let boxArea2 = calcBoxArea(30, 60);  // let boxArea2 = 1800
 console.log("box area 1:", boxArea1);
 console.log("box area 2:", boxArea2);
+*/
+
+/**
+ * Functions vs Methods
+ *
+ */
+// Methods are just functions that do something on the
+// object that they are used on. For example `.toUpperCase()`
+// that takes the value of the string it is used on and
+// returns all characters in uppercase.
+/*
+let msg = "Good Morning FED24M!";
+msg.toUpperCase();
+msg = msg.replace("Good", "Great");
+
+let username = "Johan the haxx0r";
+username.toUpperCase();
+
+msg.indexOf("0");
+username.indexOf("0");
+
+const makeMoreInteresting = (str) => {
+	return str.toUpperCase() + "!!!!!111";
+}
+
+makeMoreInteresting(username);
+
+let names = ["Kalle", "Kajsa", "Joakim", "Knatte", "Fnatte"];
+names.push("Tjatte");
+*/
+
+/*
+const finallyTalkToAReceptionist = () => {
+	alert("I LOVE JAVASCRIPT!");
+}
+
+const callClinic = (callback) => {
+	// oh noes, another telefonkö
+	for (let i = 0; i < 1000000000; i++) {
+		//console.log("beeep");
+	}
+	callback();
+}
+
+callClinic(finallyTalkToAReceptionist);
+*/
+
+/**
+ * Callbacks
+ */
+
+/*
+const logToConsole = (txt) => {
+	console.log(txt);
+}
+
+const nagUser = (txt) => {
+	alert(txt);
+}
+
+const makeMoreInteresting = (txt, callback) => {
+	const interestingText = txt + "!!!!!!!!!!!!11";
+	callback(interestingText);
+}
+
+// let msg = "I love coffee";
+// makeMoreInteresting(msg, logToConsole);
+
+makeMoreInteresting("omg omg omg", (txt) => {
+	console.log(txt);
+});
+*/
+
+let students = ["Johan", "Pelle", "Börje", "Kajan", "Tiffany"];
+//                 0        1        2        3         4
+/*
+for (let i = 0; i < students.length; i++) {
+	const student = students[i];
+	console.log(`The name of student ${i} is: ${student}`);
+}
+console.log("That's all for the 25 øre");
+*/
+
+students.push("Itzak");
+
+students.forEach( (student, index) => {
+	// this will be executed for each student
+	console.log(`The name of student ${index} is: ${student}`);
+} );
+console.log("That's all for the 25 øre");
+
+let numbers = [13, 37, 42, 1337];
+numbers.forEach((num, index) => {
+	console.log(`The number at position ${index} is: ${num}`);
+});
