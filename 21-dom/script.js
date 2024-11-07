347de1af0df51f7f47c3a1b0a215413ad43188fc
Johan Nordström <johan@digitalvillage.se>
2024-11-07 12:01:34 +0100

    🔔 Such greetings, much interactivity, very nice.

--- 21-dom/script.js
+++ 21-dom/script.js
@@ -3,6 +3,7 @@
  *
  */
 
+/*
 const firstLink = document.querySelector("#link-1");
 
 // Get the element's parent
@@ -32,3 +33,25 @@ console.log(firstErrorParagraph.innerText);
 // We can also change the content of an element
 firstParagraph.innerText = "Look mom, my first paragraph!";
 firstParagraph.innerText += " Oh I forgot this!";
+*/
+
+const alertEl = document.querySelector(".alert");
+console.log("alertEl:", alertEl);
+
+console.log("alertEl innerHTML:", alertEl.innerHTML);
+console.log("alertEl innerText:", alertEl.innerText);
+console.log("alertEl textContent:", alertEl.textContent);
+
+// Change content
+alertEl.innerText = "Such error, much danger, very fail";
+alertEl.innerText = "Such <strong>error</strong>, much danger, very fail";
+alertEl.innerHTML = "Such <strong>error</strong>, much danger, very fail";
+
+/**
+ * Say hi to the user
+ */
+const h2El = document.querySelector("h2#greetings");
+const username = prompt("What are thy name?");
+// console.log("Hello", username);
+// h2El.innerHTML = "Hello " + username;  // BIG NO-NO!
+h2El.innerText = "Hello " + username;  // yasss! <3
